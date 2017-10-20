9e5f367c8304933b9590f3598a3cced309c4e021
testgitconfig <testgitconfig>
2017-10-20 13:30:28 +0700

    555

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -10,7 +10,8 @@ import { Dialogs } from '@ionic-native/dialogs';
 })
 export class AboutPage {
 
-  data = { name:"suchada", nickname:"su" };
+  data = { name: "suchada", nickname: "su" };
+  datas: any = [];
 
   constructor(public navCtrl: NavController, private sqlite: SQLite, private toast: Toast, private dialogs: Dialogs) {
     this.getData();
@@ -25,21 +26,39 @@ export class AboutPage {
         .then(() => this.dialogs.alert('Executed SQL'))
         .catch(e => console.log(e));
     })
-    .catch(e => console.log(e));
+      .catch(e => console.log(e));
   }
 
-  addData(){
+  loadData() {
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+      .then((db: SQLiteObject) => {
+        db.executeSql('SELECT * FROM tbUser ORDER BY rowid DESC', {})
+        .then(res => {
+          this.datas = [];
+          for(let i=0; i<res.rows.length; i++){
+            this.datas.push({name: res.rows.item(i).name, nickname: res.rows.item(i).nickname});
+          }
+        })
+      })
+      .catch(e => console.log(e));
+  }
+
+  addData() {
     this.sqlite.create({
       name: 'data.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO danceMoves VALUES()', [this.data.name, this.data.nickname])
-        .then(res => 
+      db.executeSql('INSERT INTO tbUser VALUES()', [this.data.name, this.data.nickname])
+        .then(res =>
           this.dialogs.alert('Insert Into SQL')
         )
         .catch(e => console.log(e));
     })
-    .catch(e => this.dialogs.alert(e).then(() => console.log('Dialog dismissed')).catch(e => console.log('Error displaying dialog', e)));
+      .catch(e => console.log(e));
   }
+
 
 }
