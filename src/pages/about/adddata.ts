3e8ebc148aa8a9d8d9dc5a1196319b581594d821
testgitconfig <testgitconfig>
2017-10-25 14:23:47 +0700

    999

--- src/pages/about/adddata.ts
+++ src/pages/about/adddata.ts
@@ -12,16 +12,22 @@ export class AdddataPage {
 
   data = { name: "", nickname: "" };
 
-  constructor(public navCtrl: NavController,public viewCtrl: ViewController, private sqlite: SQLite, private toast: Toast, private dialogs: Dialogs) {
-    this.createTable();
-  }
+  constructor(
+    public navCtrl: NavController,
+    public viewCtrl: ViewController, 
+    private sqlite: SQLite, 
+    private toast: Toast, 
+    private dialogs: Dialogs
+  ){
+      this.createTable();
+    }
 
   createTable() {
     this.sqlite.create({
       name: 'data.db',
       location: 'default'
     }).then((db: SQLiteObject) => {
-      db.executeSql('create table tbUser(name VARCHAR(32), nickname VARCHAR(32))', {})
+      return db.executeSql('create table IF NOT EXISTS tbUser(name TEXT, nickname TEXT)', {})
         .then(() => this.dialogs.alert('Executed SQL', 'Title', 'Ok'))
         .catch(e => console.log(e));
     })
@@ -33,16 +39,19 @@ export class AdddataPage {
         name: 'data.db',
         location: 'default'
       }).then((db: SQLiteObject) => {
-        db.executeSql('INSERT INTO tbUser VALUES(NULL,?,?)', [this.data.name, this.data.nickname])
+
+        // db.executeSql('create table IF NOT EXISTS tbUser(name TEXT, nickname TEXT)', {})
+        // .then(() => this.dialogs.alert('Executed SQL', 'Title', 'Ok'))
+        // .catch(e => console.log(e));
+
+        return db.executeSql('INSERT INTO tbUser VALUES(NULL,?,?)', [this.data.name, this.data.nickname])
           .then(res =>
             //this.dialogs.alert('Insert Into SQL', 'Title', 'Ok');
             //this.viewCtrl.dismiss()
-
             this.toast.show('Data saved', '5000', 'center').subscribe(
               toast => {
                 this.viewCtrl.dismiss()
-              }
-            )
+              })
           )
           .catch(e => console.log(e));
       })
